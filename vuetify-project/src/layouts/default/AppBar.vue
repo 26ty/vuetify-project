6aab219eeaf9d0bc10b82a48855a26e0111c5b46
26ty <ch1342213422@gmail.com>
2023-07-25 15:35:00 +0800

    feat: initial commit

---
+++ vuetify-project/src/layouts/default/AppBar.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-app-bar class="">
+    <v-app-bar-title>
+      <!-- <v-icon icon="mdi-circle-slice-4" @click.stop="drawer = !drawer"/> -->
+      <v-app-bar-nav-icon color="primary" @click.stop="drawer = !drawer">
+        
+      </v-app-bar-nav-icon>
+      Base Preset
+    </v-app-bar-title>
+  </v-app-bar>
+  <v-card class="mt-3">
+    <v-layout>
+      <v-navigation-drawer v-model="drawer" temporary>
+        <v-list-item prepend-avatar="https://randomuser.me/api/portraits/men/78.jpg" title="John Leider"></v-list-item>
+
+        <v-divider></v-divider>
+
+        <v-list density="compact" nav>
+          <v-list-item prepend-icon="mdi-view-dashboard" title="Home" value="home"></v-list-item>
+          <v-list-item prepend-icon="mdi-forum" title="About" value="about"></v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+      <v-main style="height: 100vh">
+        <div class="d-flex justify-center align-center h-100">
+
+        </div>
+      </v-main>
+    </v-layout>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: null,
+    }
+  },
+}
+</script>
