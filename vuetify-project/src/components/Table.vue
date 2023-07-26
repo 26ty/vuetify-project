7260d92ac565545027566b94d8d16b727b84f017
26ty <ch1342213422@gmail.com>
2023-07-26 16:59:50 +0800

    fix: edit表格

--- vuetify-project/src/components/Table.vue
+++ vuetify-project/src/components/Table.vue
@@ -3,11 +3,17 @@
         <v-table>
             <thead>
                 <tr>
+                    <!-- <th v-for="item in desserts[0]">
+                        {{ item.name }}
+                    </th> -->
                     <th class="text-left">
                         Name
                     </th>
                     <th class="text-left">
                         Calories
+                    </th>
+                    <th class="text-left">
+                        status
                     </th>
                 </tr>
             </thead>
@@ -15,59 +21,39 @@
                 <tr v-for="item in desserts" :key="item.name">
                     <td>{{ item.name }}</td>
                     <td>{{ item.calories }}</td>
+                    <td>
+                        <v-chip v-if="item.status == false" class="ma-2" color="red" text-color="white">
+                            {{ item.status }}
+                        </v-chip>
+                        <v-chip v-else class="ma-2" color="green" text-color="white">
+                            {{ item.status }}
+                        </v-chip>
+                    </td>
                 </tr>
             </tbody>
         </v-table>
     </v-card>
-    
 </template>   
 <script>
+import desserts from '../shared/data/table/table.js'
 export default {
     data() {
+        // const keys = Object.keys(this.desserts[0])
         return {
-            desserts: [
-                {
-                    name: 'Frozen Yogurt',
-                    calories: 159,
-                },
-                {
-                    name: 'Ice cream sandwich',
-                    calories: 237,
-                },
-                {
-                    name: 'Eclair',
-                    calories: 262,
-                },
-                {
-                    name: 'Cupcake',
-                    calories: 305,
-                },
-                {
-                    name: 'Gingerbread',
-                    calories: 356,
-                },
-                {
-                    name: 'Jelly bean',
-                    calories: 375,
-                },
-                {
-                    name: 'Lollipop',
-                    calories: 392,
-                },
-                {
-                    name: 'Honeycomb',
-                    calories: 408,
-                },
-                {
-                    name: 'Donut',
-                    calories: 452,
-                },
-                {
-                    name: 'KitKat',
-                    calories: 518,
-                },
-            ],
+            // keys,
+            desserts,
+            
         }
     },
+    // 生命周期钩子会在组件生命周期的各个不同阶段被调用
+    // 例如这个函数就会在组件挂载完成后被调用
+    mounted() {
+        console.log(this.desserts[0].name)
+
+        const keys = Object.keys(this.desserts[0])
+
+        console.log(keys)
+    }
 }
+
 </script>
